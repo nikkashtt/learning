5f2633915c5fd442b573e71cc1ea68adb9a401fb
nikkashtt <nikkashtt@gmail.com>
2022-11-14 21:12:31 +0530

    initial files

--- php/php3.php
+++ php/php3.php
@@ -9,6 +9,7 @@
     $txt2 = "Kongunadu Senior Secondary School";
     $x = "13";
     $y = "8";
+    echo "I am $txt1. I am studiuying in $txt2. I am studiying grade $y. I am $x years old. "
     ?>
 </body>
 

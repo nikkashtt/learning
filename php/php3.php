49123c21f58e6e5dac25798a7be3f5a1cc11e683
nikkashtt <nikkashtt@gmail.com>
2022-12-03 20:00:56 +0530

    initial files

--- php/php3.php
+++ php/php3.php
@@ -7,9 +7,16 @@
     <?php
     $txt1 = "Nikkash";
     $txt2 = "Kongunadu Senior Secondary School";
+    $txt3 = "Obtaining the product of a sum:";
     $x = "13";
     $y = "8";
-    echo "I am $txt1. I am studiuying in $txt2. I am studiying grade $y. I am $x years old. "
+    $x1 = 1;
+    $y1 = 999999999;
+
+    echo "I am $txt1. I am studying in $txt2. I am studiying grade $y. I am $x years old.";
+    echo "<h1>" . $txt3 . "</h1>";
+    echo "1 + 999999999 = <br><br>";
+    echo $x1 + $y1;
     ?>
 </body>
 
---
+++ php/php4.php
@@ -0,0 +1,44 @@
+<html>
+    <head></head>
+<body>
+
+<?php  
+$x = 5985;
+$y = 10.365;
+$cars = array("Audi","BMW","Buggatti");
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+var_dump($cars);
+echo "<br>";
+echo "<br>";
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("white", "Audi");
+  echo $myCar -> message();
+  echo "<br>";
+  $myCar = new Car("blue", "Buggatti");
+  echo $myCar -> message();\
+  echo "<br>";
+  echo "<br>";
+  $x = "Hello world!";
+echo "<br>";
+echo "<br>";
+$x = null;
+var_dump($x);
+?>  
+
+</body>
+
+</html>

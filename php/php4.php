0116297a346823039390197a65e9aa71986a07fc
nikkashtt <nikkashtt@gmail.com>
2022-12-04 11:19:22 +0530

    initial files

--- php/php4.php
+++ php/php4.php
@@ -1,44 +1,11 @@
 <html>
-    <head></head>
 <body>
 
-<?php  
-$x = 5985;
-$y = 10.365;
-$cars = array("Audi","BMW","Buggatti");
-var_dump($x);
-echo "<br>";
-var_dump($y);
-echo "<br>";
-var_dump($cars);
-echo "<br>";
-echo "<br>";
-class Car {
-    public $color;
-    public $model;
-    public function __construct($color, $model) {
-      $this->color = $color;
-      $this->model = $model;
-    }
-    public function message() {
-      return "My car is a " . $this->color . " " . $this->model . "!";
-    }
-  }
-  
-  $myCar = new Car("white", "Audi");
-  echo $myCar -> message();
-  echo "<br>";
-  $myCar = new Car("blue", "Buggatti");
-  echo $myCar -> message();\
-  echo "<br>";
-  echo "<br>";
-  $x = "Hello world!";
-echo "<br>";
-echo "<br>";
-$x = null;
-var_dump($x);
-?>  
+<?php
+echo "Echo has no return value while print has a return value of 1 so it can be used in expressions. 
+Echo can take multiple parameters while print can take one argument. 
+Echo is marginally faster than print.";
+?> 
 
 </body>
-
 </html>

fc54eaeb0e0904b3720fda65ef039250ad22707e
nikkashtt <nikkashtt@gmail.com>
2023-04-07 19:49:26 +0530

    initial files

---
+++ php/operaters.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+
+<?php
+$x = 22;  
+$y = 2;
+echo "Adding two numbers<br>";
+echo $x + $y;
+echo "<br>";
+echo "<br>";
+echo "Subtracting two numbers<br>";
+echo $x - $y;
+echo "<br>";
+echo "<br>";
+echo "Multiplying two numbers<br>";
+echo $x * $y;
+echo "<br>";
+echo "<br>";
+echo "Dividing two numbers<br>";
+echo $x / $y;
+echo "<br>";
+echo "<br>";
+echo "Modulus<br>";
+echo $x % $y;
+echo "<br>";
+echo "<br>";
+echo "Exponentiation<br>";
+echo $x ** $y;
+?>  
+
+</body>
+</html>

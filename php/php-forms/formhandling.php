73fc0cc94532c8da003411bb0a27830aee4561af
nikkashtt <nikkashtt@gmail.com>
2023-04-22 08:46:07 +0530

    initial files

---
+++ php/php-forms/formhandling.php
@@ -0,0 +1,18 @@
+<html>  
+<body>
+<h1>PHP Form Handling:</h1>
+<h2>$_POST</h2>
+<form action="post.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+<h2>$_GET</h2>
+<form action="get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
